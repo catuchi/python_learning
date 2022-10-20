feb0572b646ce44fac50d0d2b078ad61d6d0ae36
Chibu <atuchibueze@gmail.com>
2022-10-20 12:20:59 -0400

    user_input

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+boolean_value = input("True or False?: ")
+height = float(input("How tall are you?: "))
+
+print(type(age))
+print(boolean_value)
+
