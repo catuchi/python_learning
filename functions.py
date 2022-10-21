b0304ef9a323aa9f2c95582171920bce6dbd56db
Chibu <atuchibueze@gmail.com>
2022-10-21 08:08:09 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+# function = a block of code which is executed only when it is called
+
+def greeting(first_name, last_name, age):
+  print("Hello "+first_name+" "+last_name)
+  print("You are "+str(age)+" years old")
+  print("Have a nice day!")
+
+greeting("Uncle", "Roger",30)
